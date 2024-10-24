829baaf3fe1eb46d9939d1ec1165e2b70fac9608
rediyah <mrrediya1987@gmail.com>
2024-10-24 18:10:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7vhHCAwK#NTEgfRjEru_4qTeqthHpE_tayJ1VU9JDyNTSY-0dgAI",
-MONGODB: process.env.MONGODB || "mongodb+srv://riyalrediya100:<db_password>@cluster0.ad8u5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XzA2AYLB#052_KTFfUYoclWksgvJXOQUHDSyR7RKMWWph4aVthY0",
+MONGODB: process.env.MONGODB || "mongodb+srv://mrrediya19:<db_password>@cluster0.perbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
