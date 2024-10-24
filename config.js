5cc8c2171960d3bb0c3c5f66caaed8f3e7c21302
rediyah <mrrediya1987@gmail.com>
2024-10-24 21:38:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=i6Ym0QDL#e09ORfoyqBUtXMw2Hut5UrGV_0ko8QzaQesbIFZoc7c",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=r6oXjITZ#exd0BRYHWuaLs817V4tBepg-fCeoyLMcoLf88ZPHMoI",
 MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
